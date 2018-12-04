ee7c18518e4d166194ccecc231b50157f39dd1b5
MarinaPin <marina.pinh@hotmail.com>
2018-12-03 23:09:45 -0800

    Solution for Day1 part 2

--- day1/part2.py
+++ day1/part2.py
@@ -1,11 +1,18 @@
 sum=0
-lista = []
+lista = set()
+file_contents =[]
+d=0
 
 with open("input_test.txt") as f:
     for line in f:
-        sum += int(line)
+        file_contents.append(int(line))
+
+while d <= 0:
+    for i in file_contents:
+        sum += int(i)
         if sum in lista:
             print sum
-            break 
-        lista.append(sum)
-        #print lista
+            d = 1
+            break
+        else:
+            lista.add(sum)
