2cbae2ad4b5521556f5d49022ea7bb49db587ca2
MarinaPin <marina.pinh@hotmail.com>
2018-12-09 16:10:06 -0800

    Solution for Day 3 Part 1

--- day3/part1_d3_v1.py
+++ day3/part1_d3_v1.py
@@ -1,33 +1,36 @@
 import numpy
 import re
 
+N = 1000
 
 def extract_info(line):
     j = re.findall("\d+", line)
     j = map(int, j)
-    x,y,w,h = j[1], j[2], j[3], j[4]
+    x,y,w,h =j[0], j[1], j[2], j[3], j[4]
     return x,y,w,h
 
 def fill_matrix(file_path):
-    matrix = numpy.zeros((10,10))
+    matrix = numpy.zeros((N,N))
     with open(file_path) as f:
         for line in f:
             x,y,w,h = extract_info(line)
             for j in range(y,y+h):
                 for i in range(x, x+w):
-                    if matrix[i][j] == 1:
-                        matrix[i][j] = 8
+                    if matrix[j][i] == 1 or matrix[j][i] ==8:
+                        matrix[j][i] = 8
                     else:
-                        matrix[i][j] = 1
+                        matrix[j][i] = 1
     return matrix
 
 def count_Xs(matrix):
-    #...
-
-    return num_Xs
+    n = 0
+    for j in range(N):
+        for i in range(N):
+            if matrix[i][j] == 8:
+                n += 1
+    return n
 
 matrix = fill_matrix('input.txt')
-print matrix
 
-# num_Xs = count_Xs(matrix)
-# print num_Xs
+num_Xs = count_Xs(matrix)
+print num_Xs
